f24363296c53a085044c60d30b5ce4475df5b08f
Felipe Menegueli <felipemenegueli08@gmail.com>
2022-06-23 14:17:34 -0300

    Atualizando codigo

--- Program.cs
+++ Program.cs
@@ -29,6 +29,10 @@
             string data = Console.ReadLine().ToLower();
             Console.WriteLine(data);
 
+            //outra forma de resolver sem precisar alterar as configurações do projeto.
+            //string? data = Console.ReadLine()?.ToLower();
+            //char type = char.Parse(data!.Substring(data.Length - 1, 1));
+
             //Toda metodo string tem um metodo a partir da variavel chamado SubString
             //pega uma parte da cadeia de caracteres, primeiro a parte inicial (qual caracter quer pegar), segundo a quantidade de caracteres que deseja pegar 
             char type = char.Parse(data.Substring(data.Length - 1, 1));
@@ -82,3 +86,79 @@
         }
     }
 }
+
+
+/*
+Segue codigo sem comentarios, mais limpo para uma melhor vizualização.
+
+namespace EditTexts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("-----Bem vindo ao cronometo C#-----");
+            Console.WriteLine("S = Segundos => 10s = 10 segundos");
+            Console.WriteLine("M = Minutos => 1m = 1 minuto");
+            Console.WriteLine("0 = Sair");
+            Console.WriteLine("O que deseja fazer?");
+
+            string? data = Console.ReadLine()?.ToLower();
+            Console.WriteLine(data);
+
+            char type = char.Parse(data!.Substring(data.Length - 1, 1));
+            Console.WriteLine(type);
+
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            Console.WriteLine(time);
+
+            int multiplier = 1;
+            if (type == 'm')
+
+                multiplier = 60;
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+
+        }
+        static void Start(int time)
+        {
+            int currentTime = 0;
+
+            while (time != currentTime)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+            Console.Clear();
+            Console.WriteLine("Cronometo finalizado");
+            Thread.Sleep(1500);
+            Menu();
+
+        }
+        static void PreStart(int time)
+        {
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2000);
+            Start(time);
+
+        }
+    }
+}
+
+
+
+
+*/
